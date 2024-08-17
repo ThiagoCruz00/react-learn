bd20e3a0849c876578d370cf5c772f01db3cd4a3
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-08-17 16:55:07 -0300

    feat: Criei Home-Page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header";
+import SectionHome from "../../components/section-home";
+
+export default function Home() {
+    return(
+        <>
+            <Header />
+            
+            <SectionHome />
+        </>
+    )
+}
