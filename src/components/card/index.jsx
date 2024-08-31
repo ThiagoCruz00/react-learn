4536210a1c974db697c258c66742c9d41007960a
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-08-31 16:48:02 -0300

    feat: Criação do Card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,37 @@
-import "./style.css"
+import './style.css'
+import Caixote from '../../assets/img/Caixote.svg'
 
-export default function Card(props) {
+export default function Card({title, description, scr, alt }) {
     return (
+        <>
+        <section className='carta-content'>
+            <div className='card-container'>
+               <img src={scr} alt={alt} />
+               <h2>{title}</h2>
+               <p>{description}</p>
 
-        <div className="card-container">
-            <h1>{props.title}</h1>
+            </div> 
+            
+            <div className='card-container'>
+                <img src={Caixote} alt="Imagem de uma caixa" />
+                <h2>Highlight benefit two</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique.
+                    Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+            </div> 
+            
+            <div className='card-container'>
+                <img src={Caixote} alt="Imagem de uma caixa" />
+                <h2>Highlight benefit two</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique.
+                    Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+            </div> 
+        </section>
 
-            <h1>{props.description}</h1>
+        </>
 
-            <img src={props.image} />
 
-        </div>
 
     )
 }
